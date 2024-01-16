27af706d30a661ddde599698b710cda4531f6178
mhamedsanaa <mhamed.sanaa6@gmail.com>
2024-01-16 17:19:53 +0100

    adding trigger detection and raycast

---
+++ Assets/Experiences/PointerRaycast.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class PointerRaycast : MonoBehaviour
+{
+    public XRRayInteractor rayInteractor;
+    private ActionBasedController actionController;
+    private InputHelpers.Button triggerButton = InputHelpers.Button.Trigger;
+
+    private void Awake()
+    {
+        // Get the right-hand ActionBasedController component
+        actionController = GetComponent<ActionBasedController>();
+    }
+
+    private void Update()
+    {
+        // Check if the trigger button is pressed
+        if (actionController.selectAction.action.ReadValue<float>() > 0.1f)
+        {
+            // Trigger button is pressed
+            Debug.Log("Trigger button is pressed");
+        }
+        RaycastHit res;
+        if (rayInteractor.TryGetCurrent3DRaycastHit(out res))
+        {
+            Vector3 groundPt = res.point; // the coordinate that the ray hits
+            Debug.Log(" coordinates on the ground: " + res.collider.gameObject.name);
+        }
+    }
+}

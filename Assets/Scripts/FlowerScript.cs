afc9d979e443cee31590b62b100eb7674ecb6041
elyes <elyeskacemeducation@gmail.com>
2024-01-17 11:29:49 +0100

    working on moving the player to the other scene

--- Assets/Scripts/FlowerScript.cs
+++ Assets/Scripts/FlowerScript.cs
@@ -6,9 +6,27 @@ public class FlowerScript : MonoBehaviour
 {
     public SceneTransitionManager sceneTransitionManager;
     public Vector3 firstPosition;
+
+    public GameObject player;
     // Start is called before the first frame update
+
+    public void SavePlayerTransform()
+    {
+        // Save the player's position and rotation in PlayerPrefs
+        PlayerPrefs.SetFloat("PlayerPosX", player.transform.position.x);
+        PlayerPrefs.SetFloat("PlayerPosY", player.transform.position.y);
+        PlayerPrefs.SetFloat("PlayerPosZ", player.transform.position.z);
+                                         
+        PlayerPrefs.SetFloat("PlayerRotX", player.transform.rotation.x);
+        PlayerPrefs.SetFloat("PlayerRotY", player.transform.rotation.y);
+        PlayerPrefs.SetFloat("PlayerRotZ", player.transform.rotation.z);
+        PlayerPrefs.SetFloat("PlayerRotW", player.transform.rotation.w);
+
+        PlayerPrefs.Save();
+    }
     private void Start()
     {
+        player = GameObject.Find("XR Origin (XR Rig)");
         firstPosition = new Vector3(2.45499992f, 3.80299997f, -8.55000019f);
         sceneTransitionManager = GameObject.Find("TransitionManager").GetComponent<SceneTransitionManager>();
     }
--- Assets/Scripts/GoodSceneScript.cs
+++ Assets/Scripts/GoodSceneScript.cs
@@ -5,9 +5,33 @@ using UnityEngine;
 public class GoodSceneScript : MonoBehaviour
 {
     GameObject fadeIn;
+    GameObject player;
+
+    public void LoadPlayerTransform()
+    {
+        // Check if saved player position exists
+        if (PlayerPrefs.HasKey("PlayerPosX"))
+        {
+            // Load the player's position and rotation from PlayerPrefs
+            float posX = PlayerPrefs.GetFloat("PlayerPosX");
+            float posY = PlayerPrefs.GetFloat("PlayerPosY");
+            float posZ = PlayerPrefs.GetFloat("PlayerPosZ");
+
+            float rotX = PlayerPrefs.GetFloat("PlayerRotX");
+            float rotY = PlayerPrefs.GetFloat("PlayerRotY");
+            float rotZ = PlayerPrefs.GetFloat("PlayerRotZ");
+            float rotW = PlayerPrefs.GetFloat("PlayerRotW");
+
+            // Set the player's position and rotation
+            player.transform.position = new Vector3(posX, posY, posZ);
+            player.transform.rotation = new Quaternion(rotX, rotY, rotZ, rotW);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
+        LoadPlayerTransform();
+        player = GameObject.Find("XR Origin (XR Rig)");
         fadeIn = GameObject.Find("FadeScreen");
         fadeIn.GetComponent<FadeScreen>().FadeIn();
     }

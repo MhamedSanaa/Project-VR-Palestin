410dc721deb660c03c43cc33c1b8b08289d6fc6a
elyes <elyeskacemeducation@gmail.com>
2024-01-17 11:15:43 +0100

    fixing fade in

---
+++ Assets/Scripts/GoodSceneScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodSceneScript : MonoBehaviour
+{
+    GameObject fadeIn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeIn = GameObject.Find("FadeScreen");
+        fadeIn.GetComponent<FadeScreen>().FadeIn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
